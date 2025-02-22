4c5023cd13e868f01c79613206826a575cd025ea
nihalinel <nihalinel03@gmail.com>
2025-02-21 19:55:38 -0500

    add portfolio page

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,69 @@
+import "./index.scss"
+
+// React Imports
+import React, { useState, useEffect } from "react"
+import Loader from "react-loaders"
+import AnimatedLetters from "../AnimatedLetters"
+
+// Custom Imports
+import portfolioData from '../../data/portfolio.json'
+
+const Portfolio = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 3000); //set the delay to 3 seconds
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    });
+
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className="images-container">
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div key={idx} className="image-box" >
+                                <img 
+                                src={port.cover}
+                                className="portfolio-image"
+                                alt="portfolio" />
+                                <div className="content">
+                                    <p className="title">{port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
+
+
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={"Portfolio".split("")}
+                        idx={15}
+                    />
+                </h1>
+                <div>{renderPortfolio(portfolioData.portfolio)}</div>
+            </div>
+            <Loader type="pacman" />
+        </>
+    );
+}
+
+export default Portfolio;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 import LogoN from '../../assets/images/logo_N.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faLaptop } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faLaptop, faSuitcase } from '@fortawesome/free-solid-svg-icons'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin'
@@ -26,6 +26,9 @@ const Sidebar = () => {
           </NavLink>
           <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
           <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
           </NavLink>
           <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
